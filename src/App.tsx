1aec8f2e960de491c68b64ce8d93984aa5666769
Robert Zambranom <rfzh1996@hotmail.com>
2024-08-04 23:38:43 -0500

    Update 0.0.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,41 @@
 import {
-  Admin,
-  Resource,
-  ListGuesser,
-  EditGuesser,
-  ShowGuesser,
+    Admin,
+    Resource,
+    ListGuesser,
+    EditGuesser,
+    ShowGuesser,
+    defaultLightTheme,
+    defaultDarkTheme,
 } from "react-admin";
 import { Layout } from "./Layout";
-import { dataProvider } from "./dataProvider";
+// import { dataProvider } from "./dataProvider";
+import BookIcon from "@mui/icons-material/Book";
+import ChatBubbleIcon from "@mui/icons-material/ChatBubble";
+
+import PostList from "./posts/PostList";
+import PostEdit from "./posts/PostEdit";
+import PostCreate from "./posts/PostCreate";
 
 export const App = () => (
-  <Admin layout={Layout} dataProvider={dataProvider}></Admin>
+        <Admin layout={Layout} 
+        // dataProvider={dataProvider}
+         lightTheme={defaultLightTheme} darkTheme={defaultDarkTheme}>
+            <Resource
+                name="posts"
+                list={PostList}
+                edit={PostEdit}
+                create={PostCreate}
+                show={ShowGuesser}
+                recordRepresentation="title"
+                icon={BookIcon}
+            />
+            <Resource
+                name="comments"
+                list={ListGuesser}
+                edit={EditGuesser}
+                show={ShowGuesser}
+                icon={ChatBubbleIcon}
+            />
+            <Resource name="tags" recordRepresentation={tag => tag.name.en} />
+        </Admin>
 );
--- src/dataProvider.ts
+++ src/dataProvider.ts
@@ -1,5 +1,10 @@
-import simpleRestProvider from "ra-data-simple-rest";
+// import simpleRestProvider from "ra-data-simple-rest";
+import fakeRestDataProvider from 'ra-data-fakerest';
+import data from './data.json';
 
-export const dataProvider = simpleRestProvider(
-  import.meta.env.VITE_SIMPLE_REST_URL
-);
+const dataProvider = fakeRestDataProvider(data, true);
+
+export default dataProvider;
+// export const dataProvider = simpleRestProvider(
+//   import.meta.env.VITE_SIMPLE_REST_URL
+// );
